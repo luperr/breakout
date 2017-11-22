1c029253606a8a305f1d29733732cbaa5c21ae5e
lwhiteh3 <lachlann_whitehill_93@hotmail.com>
2017-11-22 22:44:05 +1100

    First push for breakout

---
+++ compile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+g++ main.cpp -o main -lsfml-graphics -lsfml-window -lsfml-system -std=c++11
---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+using namespace sf;
+
+constexpr int windowWidth{800}, windowHeight{600};
+constexpr float ballRadius{10.f}, ballVelocity{8.f};
+
+
+struct Ball{
+	CircleShape shape;
+		Vector2f velocity{-ballVelocity, -ballVelocity};
+		Ball(float mX, float mY){
+		shape.setPosition(mX, mY);
+		shape.setRadius(ballRadius);
+		shape.setFillColor(Color::Red);
+		shape.setOrigin(ballRadius, ballRadius);
+	}
+		void update(){
+		shape.move(velocity); 
+			if(left() < 0) velocity.x = ballVelocity;
+		else if(right() > windowWidth) velocity.x = -ballVelocity;
+		
+		if(top() < 0) velocity.y = ballVelocity;
+		else if(bottom() > windowHeight) velocity.y = -ballVelocity;	
+	}
+
+	float x() {return shape.getPosition().x;}
+	float y() {return shape.getPosition().y;}
+	float left() {return x() - shape.getRadius();}
+	float right() {return x() + shape.getRadius();}
+	float top() {return y() - shape.getRadius();}
+	float bottom() {return y() + shape.getRadius();}
+};
+
+int main(){
+	RenderWindow window{{windowWidth, windowHeight}, "Breakout"};
+	window.setFramerateLimit(60);
+	Ball ball{windowWidth/2, windowHeight/2};
+
+	while(true){
+		window.clear(Color::Black);
+		
+		if(Keyboard::isKeyPressed(Keyboard::Key::Escape)) break;
+		
+		ball.update();
+		
+		window.draw(ball.shape);
+		window.display();
+	}
+
+	return 0;
+}
